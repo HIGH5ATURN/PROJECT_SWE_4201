ab44357b9bd5e96040f95dcc5c5217a62f830816
KashshafLabib <kashshaflabib@iut-dhaka.edu>
2023-04-26 18:57:50 +0600

    changed Society.cs and Student.cs

--- IUTSMS(MAIN)/Society.cs
+++ IUTSMS(MAIN)/Society.cs
@@ -63,16 +63,185 @@ namespace IUTSMS_MAIN_
     }
     public class IUTDS : Society
     {
-        public static List<student> ds_students = new List<student>();
+        public static List<ds_student> arr_ds_students = new List<ds_student>();
+
+
+
+        OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OleDb.16.0; Data Source = dbst.accdb");
+
+
+        OleDbCommand cmd = new OleDbCommand();
+
+
+        public void fill_ds_arr()
+        {
+            arr_ds_students.Clear();
+
+
+
+
+            try
+            {
+
+
+                conn.Open();
+
+
+                string t = "Select * from ds_table";
+
+                cmd = new OleDbCommand(t, conn);
+                OleDbDataReader dr;
+
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+
+                    ds_student st = new ds_student(dr["naam"].ToString(), dr["dept"].ToString(), Convert.ToInt32(dr["st_id"].ToString()));
+                    arr_ds_students.Add(st);
+
+                }
+
+
+
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+
+        }
     }
 
     public class IUTPS : Society
     {
-        public static List<student> ps_students = new List<student>();
+        public static List<ps_student> arr_ps_students = new List<ps_student>();
+
+
+
+        OleDbConnection conn = new OleDbConnection("\"Provider=Microsoft.ACE.OleDb.16.0; Data Source = dbst.accdb\"");
+
+
+        OleDbCommand cmd=new OleDbCommand();
+
+
+
+        public void fill_ps_arr()
+        {
+            arr_ps_students.Clear();
+
+
+
+
+
+
+            try
+            {
+
+
+                conn.Open();
+
+
+                string t = "Select * from ps_table";
+
+                cmd = new OleDbCommand(t, conn);
+                OleDbDataReader dr;
+
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+
+                    ps_student st = new ps_student(dr["naam"].ToString(), dr["dept"].ToString(), Convert.ToInt32(dr["st_id"].ToString()), dr["cam"].ToString().ToLower(), dr["adobe"].ToString().ToLower(), dr["graph"].ToString().ToLower());
+
+
+
+                    arr_ps_students.Add(st);
+
+                }
+
+                conn.Close();
+            }
+
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
     }
     public class IUTSIKS : Society
     {
-        public static List<student> siks_students = new List<student>();
+        public static List<siks_student> arr_siks_students = new List<siks_student>();
+
+
+
+
+        OleDbConnection conn = new OleDbConnection("\"Provider=Microsoft.ACE.OleDb.16.0; Data Source = dbst.accdb\"");
+
+
+        OleDbCommand cmd = new OleDbCommand();
+
+
+
+        public void fill_siks_arr()
+        {
+            arr_siks_students.Clear();
+
+
+
+            try
+            {
+
+
+                conn.Open();
+
+
+                string t = "Select * from siks_table";
+
+                cmd = new OleDbCommand(t, conn);
+                OleDbDataReader dr;
+
+
+
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+
+                    siks_student st = new siks_student(dr["naam"].ToString(), dr["dept"].ToString(), Convert.ToInt32(dr["st_id"].ToString()));
+
+
+
+                    arr_siks_students.Add(st);
+
+                }
+
+                conn.Close();
+            }
+
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+
     }
 
 
--- IUTSMS(MAIN)/student.cs
+++ IUTSMS(MAIN)/student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,5 +42,73 @@ namespace IUTSMS_MAIN_
             this.webdev = webdev;
         }
     }
+
+
+
+
+
+    public class siks_student : student
+    {
+        public siks_student(string name,string department,int id) :base(name,department,id)
+        {
+            this.name = name;
+
+            this.department = department;
+
+            this.id = id;
+        }
+    }
+
+
+
+
+
+    public class ps_student : student
+    {
+        public string cam { get; set; }
+        
+        public string adobe { get; set; }
+
+        public string graph { get; set; }
+
+
+        public ps_student(string name, string department, int id,string cam,string adobe,string graph):base(name,department,id)
+        {
+            this.name = name;
+
+            this.department = department;
+
+            this.id = id;
+
+            this.cam = cam;
+
+            this.adobe = adobe;
+
+            this.graph = graph;
+        }
+
+
+    }
+
+
+
+
+
+    public class ds_student : student
+    {
+
+        public ds_student(string name,string department,int id):base(name,department,id)
+        {
+
+            this.name = name;
+
+            this.department = department;
+
+            this.id = id;
+        }
+
+    }
+
+
 
 }
